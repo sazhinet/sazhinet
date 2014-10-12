8cc0ff4e8cc8c0fbba7dca4656b51bbf3cb772ec
Danil Kutkevich <danil@kutkevich.org>
2014-10-12 19:32:22 +0000

    add main background image

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var jade = require('gulp-jade');
 gulp.task('copy', function() {
   gulp.src('./app/images/favicon.ico')
     .pipe(gulp.dest("./dist/"));
-  gulp.src('./app/images/logo_main.png')
+  gulp.src('./app/images/*.{png,jpg,gif}')
     .pipe(gulp.dest("./dist/images"));
   gulp.src('./app/css/*.css')
     .pipe(gulp.dest("./dist/assets/"));
