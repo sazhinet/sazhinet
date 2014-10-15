9870687ffa0b4480e8e598b8e765a27b2b36084f
Danil Kutkevich <danil@kutkevich.org>
2014-10-15 20:13:52 +0000

    uglify and concat js

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var jade = require('gulp-jade');
 var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 var paths = {
   css: './app/css/*.css',
   dist: './dist',
   images: './app/images',
-  jade: './app/jade/*.jade'
+  jade: './app/jade/*.jade',
+  js: './app/js/*.js'
 };
 
 gulp.task('images', function() {
@@ -27,8 +29,15 @@ gulp.task('images', function() {
 
 gulp.task('css', function() {
   gulp.src(paths.css)
-    .pipe(concat('all.css'))
-    .pipe(minifyCSS({keepBreaks:true})) //until start using bootstrap
+    .pipe(minifyCSS({keepBreaks:true})) //keepBreaks until start using bootstrap
+    .pipe(concat('all.min.css'))
+    .pipe(gulp.dest(paths.dist + '/assets/'));
+});
+
+gulp.task('js', function() {
+  gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
     .pipe(gulp.dest(paths.dist + '/assets/'));
 });
 
@@ -42,4 +51,4 @@ gulp.task('jade', function() {
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('default', ['jade', 'images', 'css']);
+gulp.task('default', ['jade', 'images', 'css', 'js']);
